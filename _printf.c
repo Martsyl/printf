d60d3e0d006b8a341095c5100464b571cfb4a8b1
Martsyl <msnonuorah3964@gmail.com>
2023-06-26 23:45:20 -0700

    ww2

--- _printf.c
+++ _printf.c
@@ -30,9 +30,10 @@ void str_handle(va_list list, int *count)
 	while (prnt_str[get_len] != '\0')
 	{
 		get_len++;
+		count++;
 	}
 	write(1, prnt_str, get_len);
-	count += get_len;
+	(*count) += get_len;
 }
 /**
  *prcnt_handle-prints%
